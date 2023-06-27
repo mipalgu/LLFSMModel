91119041de26bfc5d569a6f507b5e5120a0f1448
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-06-27 19:42:46 +1000

    Initialised swift package.

---
+++ Sources/LLFSMModel/LLFSMModel.swift
@@ -0,0 +1,6 @@
+public struct LLFSMModel {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/LLFSMModelTests/LLFSMModelTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import LLFSMModel
+
+final class LLFSMModelTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(LLFSMModel().text, "Hello, World!")
+    }
+}
