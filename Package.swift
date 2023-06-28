faa86844d7ebb373e53488b622d07c6a6a7c8cdc
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-06-29 03:27:09 +1000

    Fixed formatting.

--- Package.swift
+++ Package.swift
@@ -3,13 +3,16 @@
 
 import PackageDescription
 
+/// The package definition.
 let package = Package(
     name: "LLFSMModel",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible to other
+        // packages.
         .library(
             name: "LLFSMModel",
-            targets: ["LLFSMModel"]),
+            targets: ["LLFSMModel"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,12 +21,15 @@ let package = Package(
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages this package
+        // depends on.
         .target(
             name: "LLFSMModel",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "LLFSMModelTests",
-            dependencies: ["LLFSMModel"]),
+            dependencies: ["LLFSMModel"]
+        )
     ]
 )
